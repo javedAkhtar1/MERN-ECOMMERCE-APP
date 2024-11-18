98a9242c4cc1213369b135923c3a2d6f41f31638
Javed Akhtar <jved004@gmail.com>
2024-11-18 14:00:32 +0530

    complete product page done

--- FRONTEND/src/components/CompleteProduct.jsx
+++ FRONTEND/src/components/CompleteProduct.jsx
@@ -3,11 +3,16 @@ import { useParams } from "react-router";
 import { products } from "../shop.js";
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
-import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import FeaturedProductCard from "./FeaturedProductCard.jsx";
+import { Link } from "react-router-dom";
 
 function CompleteProduct() {
   const { id } = useParams();
   const product = products.find((p) => p.id === parseInt(id));
+  const similarProducts = products.filter(
+    (p) => p.category === product.category && p.id !== product.id
+  );
 
   if (!product) {
     return <p>OOPS! Product not found!</p>;
@@ -55,24 +60,47 @@ function CompleteProduct() {
 
       {/* For smaller than lg */}
       <div className="flex flex-col gap-2 p-5 items-center justify-center max-w-5xl mx-auto lg:hidden">
-          <h2>About this product:</h2>
-          <p className="text-sm text-gray-500">{product.about} </p>
+        <h2>About this product:</h2>
+        <p className="text-sm text-gray-500">{product.about} </p>
       </div>
 
       {/* Reviews */}
-        <h1 className="text-center text-3xl font-Nunito text-gray-700 border-b max-w-5xl mx-auto p-3">Reviews</h1>
-        {product.reviews.map((r) => (
-          <>
-            <div className="max-w-5xl mx-auto px-5 py-3 border-b">
-              <div className="flex items-center py-2 text-lg font-Nunito gap-1"> 
-              <AccountCircleRoundedIcon className="text-xs text-gray-500"/> 
+      <h1 className="text-center text-3xl font-Nunito text-gray-700 border-b max-w-5xl mx-auto p-3">
+        Reviews
+      </h1>
+      {product.reviews.map((r) => (
+        <>
+          <div className="max-w-5xl mx-auto px-5 py-3 border-b">
+            <div className="flex items-center py-2 text-lg font-Nunito gap-1">
+              <AccountCircleRoundedIcon className="text-xs text-gray-500" />
               <p>{r.username}</p>
-              </div>
-              <p className="px-2 text-sm">{r.review}</p>
             </div>
-          </>
-        ))}
+            <p className="px-2 text-sm">{r.review}</p>
+          </div>
+        </>
+      ))}
 
+      {/* You might also like */}
+      <h1 className="text-center font-light mt-6 text-2xl font-Nunito text-red-900 border-b max-w-5xl mx-auto p-3">
+            YOU MIGHT ALSO LIKE
+      </h1>
+      <div className="max-w-5xl mx-auto p-5 pb-0 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-9">
+      {similarProducts.map((product) => (
+        <>
+          <div className="flex flex-col gap-2 items-center">
+            <Link to={`/shop/product/${product.id}`} className="h-[200px] w-[200px]">
+              <img
+                src={product.image}
+                alt={`${product.image} image`}
+                className="h-[200px] w-[200px]"
+              />
+            </Link>
+            <p className="">{product.name}</p>
+            <p className="font-Rubik">Rs. {product.price}</p>
+          </div>
+        </>
+      ))}
+      </div>
       <Footer />
     </>
   );
--- FRONTEND/src/shop.js
+++ FRONTEND/src/shop.js
@@ -3,6 +3,7 @@ export const products = [
         id: 1,
         name: "Mens's Denim Jacket",
         price: '2,999',
+        category: "men",
         image: "/featuredProductsImages/men-denim-jacket.jpg",
         description: "Stylish piece of clothing for men",
         rating: '4.5',
@@ -27,6 +28,7 @@ export const products = [
         id: 2,
         name: "Floral Dress for women",
         price: '1,749',
+        category: "women",
         image: "/featuredProductsImages/floral-dress.jpg",
         description: "Summer essentials for women",
         about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
@@ -49,6 +51,7 @@ export const products = [
         id: 3,
         name: "Black Leather Belt",
         price: '899',
+        category: "men",
         image: "/featuredProductsImages/leather-belt.jpg",
         description: "Premium quality belts for men",
         about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
@@ -71,6 +74,7 @@ export const products = [
         id: 4,
         name: "Kids T-Shirt",
         price: '599',
+        category: "kids",
         image: "/featuredProductsImages/kids-tshirt.jpg",
         description: "Plain T-Shirt for kids",
         about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
@@ -93,6 +97,7 @@ export const products = [
         id: 5,
         name: "Women's Sandals",
         price: '1,599',
+        category: "women",
         image: "/featuredProductsImages/women-sandal.jpg",
         description: "Stylish Sandal for women",
         about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
@@ -512,6 +517,121 @@ export const products = [
         name: "Blue Men's Blazer",
         price: '1,679',
         image: "/featuredProductsImages/blue-men-blazer.jpg",
+        description: "Essential piece of clothing for men",
+        about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
+        reviews: [
+          {
+            username: "Superman",
+            review: "nice product"
+          },
+          {
+            username: "Gaurav",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea dignissimos incidunt ut a, earum nostrum labore hic modi ft debitis."
+          },
+          {
+            username: "Javed",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea digni."
+          },
+        ]
+      },
+      {
+        id: 7,
+        name: "Jeans",
+        price: '1,299',
+        image: "/featuredProductsImages/blue-men.jpg",
+        category: "men",
+        description: "Essential piece of clothing for men",
+        about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
+        reviews: [
+          {
+            username: "Superman",
+            review: "nice product"
+          },
+          {
+            username: "Gaurav",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea dignissimos incidunt ut a, earum nostrum labore hic modi ft debitis."
+          },
+          {
+            username: "Javed",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea digni."
+          },
+        ]
+      },
+      {
+        id: 8,
+        name: "Shirt",
+        price: '1,699',
+        image: "/featuredProductsImages/blue-men.jpg",
+        category: "men",
+        description: "Essential piece of clothing for men",
+        about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
+        reviews: [
+          {
+            username: "Superman",
+            review: "nice product"
+          },
+          {
+            username: "Gaurav",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea dignissimos incidunt ut a, earum nostrum labore hic modi ft debitis."
+          },
+          {
+            username: "Javed",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea digni."
+          },
+        ]
+      },
+      {
+        id: 9,
+        name: "Kurta",
+        price: '2,299',
+        image: "/featuredProductsImages/blue-men.jpg",
+        category: "men",
+        description: "Essential piece of clothing for men",
+        about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
+        reviews: [
+          {
+            username: "Superman",
+            review: "nice product"
+          },
+          {
+            username: "Gaurav",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea dignissimos incidunt ut a, earum nostrum labore hic modi ft debitis."
+          },
+          {
+            username: "Javed",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea digni."
+          },
+        ]
+      },
+      {
+        id: 10,
+        name: "Cap",
+        price: '299',
+        image: "/featuredProductsImages/blue-men.jpg",
+        category: "men",
+        description: "Essential piece of clothing for men",
+        about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
+        reviews: [
+          {
+            username: "Superman",
+            review: "nice product"
+          },
+          {
+            username: "Gaurav",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea dignissimos incidunt ut a, earum nostrum labore hic modi ft debitis."
+          },
+          {
+            username: "Javed",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus facilis ea digni."
+          },
+        ]
+      },
+      {
+        id: 11,
+        name: "Shorts",
+        price: '469',
+        image: "/featuredProductsImages/blue-men.jpg",
+        category: "men",
         description: "Essential piece of clothing for men",
         about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis vero nisi ullam incidunt dignissimos tenetur itaque quae veniam, amet et a repudiandae, rerum eveniet ea odio sapiente non distinctio saepe.",
         reviews: [
